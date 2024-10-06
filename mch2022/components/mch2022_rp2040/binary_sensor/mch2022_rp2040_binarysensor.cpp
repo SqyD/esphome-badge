34bb076a00228bdab592870326a25df9be1c50f7
Paul Krischer <paul@krischer.nl>
2024-10-07 00:08:31 +0200

    Adding setup method for binary sensors.

--- mch2022/components/mch2022_rp2040/binary_sensor/mch2022_rp2040_binarysensor.cpp
+++ mch2022/components/mch2022_rp2040/binary_sensor/mch2022_rp2040_binarysensor.cpp
@@ -6,6 +6,17 @@ namespace mch2022_rp2040 {
 
 static const char *const TAG = "mch2022_rp2040.binary_sensor";
 
+void MMch2022_rp2040BinarySensor::setup() {
+  ESP_LOGCONFIG(TAG, "Setting up the MCH2022 Badge RP2040 binary sensors...");
+
+  // Can we read an input register?
+  uint8_t port = 0;
+  if (this->parent->read_register(RP2040_REG_INPUT1, &port, 1) != i2c::ERROR_OK) {
+    this->mark_failed();
+    return;
+  }
+}
+
 void Mch2022_rp2040BinarySensor::dump_config() { LOG_BINARY_SENSOR("", "MCH2022 RP2040 Binary Sensor", this); }
 
 
