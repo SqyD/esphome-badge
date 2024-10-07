16cae2d71e30acd294777b567fe375b77e6d032a
Paul Krischer <paul@krischer.nl>
2024-10-07 13:00:48 +0200

    CV is stil messy.

--- mch2022/components/mch2022_rp2040/__init__.py
+++ mch2022/components/mch2022_rp2040/__init__.py
@@ -28,7 +28,7 @@ CONFIG_SCHEMA = (
     cv.Schema({cv.Required(CONF_ID): cv.declare_id(Mch2022_rp2040Component)})
     .extend(cv.COMPONENT_SCHEMA)
     .extend(i2c.i2c_device_schema(0x17))
-    .extend( { cv.Optional(CONF_INTERRUPT_PIN, default="34"): pins.internal_gpio_input_pin_schema })
+    .extend({cv.Optional(CONF_INTERRUPT_PIN, default="34"): cv.All(pins.internal_gpio_input_pin_schema)})
 )
 
 async def to_code(config):
@@ -36,7 +36,7 @@ async def to_code(config):
         config[CONF_ID],
         # config[CONF_INTERRUPT_PIN]
         )
-    cg.add(var.config(CONF_INTERRUPT_PIN))
+    # cg.add(var.config(CONF_INTERRUPT_PIN))
     await cg.register_component(var, config)
     await i2c.register_i2c_device(var, config)
 
