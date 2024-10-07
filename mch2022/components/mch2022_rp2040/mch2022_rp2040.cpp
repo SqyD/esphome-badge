ae5e127bdaebc906640a10c66cd74a62970364af
Paul Krischer <paul@krischer.nl>
2024-10-07 22:17:01 +0200

    write back to interup register.

--- mch2022/components/mch2022_rp2040/mch2022_rp2040.cpp
+++ mch2022/components/mch2022_rp2040/mch2022_rp2040.cpp
@@ -32,6 +32,7 @@ void Mch2022_rp2040Component::loop() {
     if (!this->interrupt_pin_->digital_read()) {
       uint8_t reg1 = 0; 
       this->read_register(RP2040_REG_INPUT1, &reg1, 2);
+      this->write_register(RP2040_REG_INTERRUPT2, 0x00)
     // uint8_t reg2 = this->read_register(RP2040_REG_INPUT1, &port, 1);
     // ESP_LOGE(TAG, "Button pressed on the rp2040 !");
        ESP_LOGE(TAG, "Reg1: %i", reg1);
