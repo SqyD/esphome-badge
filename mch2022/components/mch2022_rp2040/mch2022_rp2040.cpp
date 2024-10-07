c0f84242ad74ab2b32af514abb94de47121a70c1
Paul Krischer <paul@krischer.nl>
2024-10-07 21:10:56 +0200

    undoing the uninterupt.

--- mch2022/components/mch2022_rp2040/mch2022_rp2040.cpp
+++ mch2022/components/mch2022_rp2040/mch2022_rp2040.cpp
@@ -34,7 +34,7 @@ void Mch2022_rp2040Component::loop() {
       this->read_register(RP2040_REG_INPUT1, &reg1, 2);
     // uint8_t reg2 = this->read_register(RP2040_REG_INPUT1, &port, 1);
     // ESP_LOGE(TAG, "Button pressed on the rp2040 !");
-       ESP_LOGE(TAG, "Reg1: %i", (unint16_t) reg1);
+       ESP_LOGE(TAG, "Reg1: %i", (uint16_t) reg1);
     }
     // ESP_LOGE(TAG, "Reg2: %i", (int) reg2);
     // if (!this->interrupt_pin_->digital_read()) {
