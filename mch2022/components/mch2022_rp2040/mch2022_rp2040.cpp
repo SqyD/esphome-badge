309c97a64c744f7721b9f18d272d447cfdeaebe0
Paul Krischer <paul@krischer.nl>
2024-10-09 09:44:26 +0200

    declaring variables fixed.

--- mch2022/components/mch2022_rp2040/mch2022_rp2040.cpp
+++ mch2022/components/mch2022_rp2040/mch2022_rp2040.cpp
@@ -32,6 +32,7 @@ void Mch2022_rp2040Component::dump_config() {
 
 void Mch2022_rp2040Component::loop() {
     bool current_interrupt = !this->interrupt_pin_->digital_read();
+    uint8_t input_register = 0x00;
     if (this->button_interrupt_ != current_interrupt) {
         this->button_interrupt_ = current_interrupt;
         ESP_LOGE(TAG, "Interrupt changed on the rp2040 !");
