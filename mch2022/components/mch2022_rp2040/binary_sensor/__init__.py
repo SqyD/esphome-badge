680345c68c6372cf3781341b75d6af90ff09f4b7
Paul Krischer <paul@krischer.nl>
2024-10-09 19:41:21 +0200

    Registration woes.

--- mch2022/components/mch2022_rp2040/binary_sensor/__init__.py
+++ mch2022/components/mch2022_rp2040/binary_sensor/__init__.py
@@ -42,13 +42,13 @@ CONFIG_SCHEMA = cv.Schema(
 
 
 async def to_code(config):
-    #var = cg.new_Pvariable(
-    ##    config[CONF_ID],
-    #  config[CONF_INPUT]
-    #)
+    var = cg.new_Pvariable(
+      config[CONF_ID],
+      config[CONF_INPUT]
+    )
     paren = await cg.get_variable(config[CONF_MCH20222_RP2040])
     sens = await binary_sensor.new_binary_sensor(config)
     input = CONF_INPUTS[config[CONF_INPUT]]
     cg.add(paren.set_sub_binary_sensor(input, sens))
-    # await cg.register_component(var, config)
+    await cg.register_component(var, config)
  
