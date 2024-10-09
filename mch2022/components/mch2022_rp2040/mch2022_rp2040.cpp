df1a230094a49e501cdd66188e67252953e98a42
Paul Krischer <paul@krischer.nl>
2024-10-09 13:15:40 +0200

    Cleanup

--- mch2022/components/mch2022_rp2040/mch2022_rp2040.cpp
+++ mch2022/components/mch2022_rp2040/mch2022_rp2040.cpp
@@ -8,7 +8,6 @@ static const char *const TAG = "mch2022_rp2040";
 
 void Mch2022_rp2040Component::setup() {
   ESP_LOGCONFIG(TAG, "Setting up the MCH2022 Badge RP2040 io expander component...");
-
   // Can we read an input register?
   uint8_t port = 0;
   if (this->read_register(RP2040_REG_INPUT1, &port, 1) != i2c::ERROR_OK) {
@@ -18,12 +17,12 @@ void Mch2022_rp2040Component::setup() {
   this->interrupt_pin_->setup();
   this->input_interrupt_ = false;
   this->input_state_ = 0x0000;
+  this->update_inputs()
 }
 
 void Mch2022_rp2040Component::dump_config() {
   ESP_LOGCONFIG(TAG, "mch2022_rp2040:");
   LOG_I2C_DEVICE(this);
-
   if (this->is_failed()) {
     ESP_LOGE(TAG, "Communication with the rp2040 failed!");
   }
@@ -33,10 +32,8 @@ void Mch2022_rp2040Component::dump_config() {
 
 void Mch2022_rp2040Component::update_interrupt() {
   bool current_interrupt = !this->interrupt_pin_->digital_read();
-
   if (this->input_interrupt_ != current_interrupt) {
     this->input_interrupt_ = current_interrupt;
-    // ESP_LOGD(TAG, "Interrupt changed on the rp2040 !");
   }
 }
 
@@ -58,48 +55,6 @@ void Mch2022_rp2040Component::loop() {
     this->update_inputs();
   }
 }
-
-
-    // if (!this->interrupt_pin_->digital_read()) {
-    //  uint8_t reg1 = 0;
-    //  bool stop = true;
-    //  this->read_register(RP2040_REG_INPUT1, &reg1, 4, stop);
-      // this->write_register(RP2040_REG_INTERRUPT2, &reg1, 2, stop);
-    // uint8_t reg2 = this->read_register(RP2040_REG_INPUT1, &port, 1);
-    // ESP_LOGE(TAG, "Button pressed on the rp2040 !");
-    //  ESP_LOGE(TAG, "Reg1: %i", reg1);
-    //}
-    // ESP_LOGE(TAG, "Reg2: %i", (int) reg2);
-    // if (!this->interrupt_pin_->digital_read()) {
-    // if {
-
-    //  ESP_LOGE(TAG, "Button pressed on the rp2040 !");
-    //}
-    // this->interrupt_pin->clear_interrupt()
-
-
-//void Mch2022_rp2040Component::pin_mode(uint8_t pin, gpio::Flags mode) {
-//  uint8_t port = 0;
-//
-//  this->read_register(RP2040_REG_GPIO_DIR, &port, 1);
-//
-//  if (mode == gpio::FLAG_INPUT) {
-//    port = port | (1 << (pin - 10));
-//  } else if (mode == gpio::FLAG_OUTPUT) {
-//    port = port & (~(1 << (pin - 10)));
-//  }
-//
-//  this->write_register(RP2040_REG_GPIO_DIR, &port, 1);
-//
-//}
-
-//void Mch2022_rp2040GPIOPin::setup() { this->pin_mode(this->flags_); }
-
-//std::string Mch2022_rp2040GPIOPin::dump_summary() const { return str_snprintf("%u via Mch2022 RP2040", 15, this->pin_); }
-
-// void Mch2022_rp2040GPIOPin::pin_mode(gpio::Flags flags) { this->parent_->pin_mode(this->pin_, flags); }
-// bool Mch2022_rp2040GPIOPin::digital_read() { return this->parent_->digital_read(this->pin_) != this->inverted_; }
-// void Mch2022_rp2040GPIOPin::digital_write(bool value) { this->parent_->digital_write(this->pin_, value != this->inverted_); }
 
 }  // namespace mch2022_rp2040
 }  // namespace esphome
