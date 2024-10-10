b7d9061d1415b4ca919a5a3775afdecac4a24e62
Paul Krischer <paul@krischer.nl>
2024-10-10 21:27:32 +0200

    Converting input index to string.

--- mch2022/components/mch2022_rp2040/binary_sensor.py
+++ mch2022/components/mch2022_rp2040/binary_sensor.py
@@ -14,18 +14,18 @@ BinarySensorInputEnum = Mch2022_rp2040Component.enum("SubBinarySensorInput", Tru
 ## MULTI_CONF = True
 
 CONF_INPUTS = {
-  "button_home": 0,
-  "button_menu": 1,
-  "button_start": 2,
-  "button_accept": 3,
-  "button_back": 4,
-  "battery_charging": 6,
-  "button_select": 7,
-  "joystick_left": 8,
-  "joystick_press": 9,
-  "joystick_down": 10,
-  "joystick_up": 11,
-  "joystick_right": 12
+  "button_home": "0",
+  "button_menu": "1",
+  "button_start": "2",
+  "button_accept": "3",
+  "button_back": "4",
+  "battery_charging": "6",
+  "button_select": "7",
+  "joystick_left": "8",
+  "joystick_press": "9",
+  "joystick_down": "10",
+  "joystick_up": "11",
+  "joystick_right": "12"
 }
 
 CONFIG_SCHEMA = cv.Schema(
