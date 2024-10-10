2c6da69a008b6c263c3cc6c0645fa64d399f88cc
Paul Krischer <paul@krischer.nl>
2024-10-10 16:17:42 +0200

    Just trying other variations.

--- mch2022/components/mch2022_rp2040/binary_sensor.py
+++ mch2022/components/mch2022_rp2040/binary_sensor.py
@@ -46,7 +46,7 @@ async def to_code(config):
       config[CONF_INPUT]
     )
     # paren = await cg.get_variable(config[CONF_MCH20222_RP2040])
-    sens = await binary_sensor.new_binary_sensor(config)
+    # sens = await binary_sensor.new_binary_sensor(config)
     #input_int = CONF_INPUTS[config[CONF_INPUT]]
     #cg.add(paren.set_sub_binary_sensor(input_int, sens))
     await cg.register_component(var, config)
