0ed9eda029f4f4aa8a2bcc4165bafe1fa024bf90
Paul Krischer <paul@krischer.nl>
2024-10-10 15:53:04 +0200

    Still figuring out the schema

--- mch2022/components/mch2022_rp2040/binary_sensor.py
+++ mch2022/components/mch2022_rp2040/binary_sensor.py
@@ -31,9 +31,11 @@ CONFIG_SCHEMA = cv.Schema(
   .extend(
       {
         ## cv.GenerateID(CONF_MCH20222_RP2040): cv.use_id(Mch2022_rp2040Component),
+        
         cv.Required(CONF_INPUT): cv.string,
       }
   )
+  .extend(cv.COMPONENT_SCHEMA)
 )
 
 
