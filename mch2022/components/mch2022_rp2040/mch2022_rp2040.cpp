b7cae797c6812c64bc1ab1dbe0085e3d842d40c8
Paul Krischer <paul@krischer.nl>
2024-10-11 03:05:06 +0200

    Rare jongens die romeinen.

--- mch2022/components/mch2022_rp2040/mch2022_rp2040.cpp
+++ mch2022/components/mch2022_rp2040/mch2022_rp2040.cpp
@@ -31,7 +31,7 @@ void Mch2022_rp2040Component::dump_config() {
 }
 
 #ifdef USE_BINARY_SENSOR
-void Mch2022_rp2040Component::set_sub_binary_sensor(size_t input, binary_sensor::BinarySensor *sens) {
+void Mch2022_rp2040Component::set_sub_binary_sensor(size_t input, binary_sensor::BinarySensor sens) {
 // void Mch2022_rp2040Component::set_sub_binary_sensor(uint8_t input, binary_sensor::BinarySensor *sens) {
   // if (input < SubBinarySensorInput::SUB_BINARY_SENSOR_INPUT_COUNT) {
   this->sub_binary_sensors_[(size_t)input] = sens;
